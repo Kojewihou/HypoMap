00f080bd44d376b9dc30500f61a36bfa618f0a0b
kojewihou <103379288+ehvr20@users.noreply.github.com>
2023-12-13 14:11:04 +0000

    fix extract_starsolo_results to .sh

---
+++ scripts/setup/setup_smartseq.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p data scripts temp logs results
+
+repo="https://raw.githubusercontent.com/ehvr20/HypoMap/main/"
+
+function get_repo_file () {
+	filepath=$1
+	output_dir=$2
+	filename=$(basename $filepath)
+	if [ ! -e "$output_dir/$filename" ];then
+  	      wget -P $output_dir "$repo/$filepath"
+	else
+	      echo "$repo/$filename already retrieved"
+	fi
+}
+
+get_repo_file "scripts/utils/matrix_h5_format.py" "scripts/"
+get_repo_file "scripts/utils/link_data.sh" "scripts/"
+get_repo_file "scripts/utils/extract_starsolo_results.sh" "scripts/"
+get_repo_file "scripts/smartseq/generate_manifest-ss.sh" "scripts/"
+get_repo_file "scripts/smartseq/slurm-starsolo-ss.sh" "scripts/"
+get_repo_file "scripts/smartseq/slurm-bam_processing-ss.sh" "scripts/"
+get_repo_file "scripts/smartseq/run_smartseq.sh" "."
+
+
+echo "$0 finished!"
+exit 0
