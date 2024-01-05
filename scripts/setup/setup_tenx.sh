29471cec7587d465f46d69e53c454ce39d88da5f
kojewihou <103379288+ehvr20@users.noreply.github.com>
2024-01-05 14:35:15 +0000

    tenX complete

---
+++ scripts/setup/setup_tenx.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+mkdir -p data scripts temp logs results
+
+repo="https://raw.githubusercontent.com/ehvr20/HypoMap/main/"
+
+function get_repo_file () {
+        filepath=$1
+        output_dir=$2
+        filename=$(basename $filepath)
+        if [ ! -e "$output_dir/$filename" ];then
+              wget -P $output_dir "$repo/$filepath"
+        else
+              echo "$output_dir/$filename already retrieved"
+        fi
+}
+
+get_repo_file "scripts/utils/link_data.sh" "scripts/"
+get_repo_file "scripts/utils/generate_sample_list.sh" "scripts/"
+
+get_repo_file "scripts/tenx/rename_10x_files.sh" "scripts/"
+get_repo_file "scripts/tenx/extract_10x_results.sh" "scripts/"
+get_repo_file "scripts/tenx/slurm-cellranger.sh" "scripts/"
+
+get_repo_file "scripts/tenx/run_tenx.sh" "."
+
+
+
+echo "$0 finished!"
+exit 0
